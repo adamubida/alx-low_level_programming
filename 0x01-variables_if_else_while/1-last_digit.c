58d50db5ce5d9612dc6e2bae3b332b1302f541f8
adamubida <mailadamu@gmail.com>
2022-09-10 15:44:03 +0100

    diglast

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
 /**
- * main - Entry point
- *
- *  Return: Always 0 (Success)
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
  */
 int main(void)
 {
 	int n;
-	int L;
-	char str[] = "Last digit of";
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	L = n % 10;
+	last = n % 10;
 
-	if (L > 5)
-		printf("%s %d is %d and is greater than 5\n", str, n, L);
-	else if (L == 0)
+	if (last > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %i is %i and is 0\n", n, last);
+	else if (last < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 
-		printf("%s %d is %d and is 0\n", str, n, L)
-			else if (L < 6)
-				printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
-
-			return(0)
+	return (0);
 }
