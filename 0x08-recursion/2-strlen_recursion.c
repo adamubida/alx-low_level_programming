3510b61b3e7f947850a069c71c558f999342290f
adamubida <mailadamu@gmail.com>
2022-09-28 15:28:00 +0100

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns Length of String
- * @s: string
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _strlen_recursion - note: no _putchar in local test
+ * @s: input
+ * Return: Always 0 (Success)
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
+	if (*s == '\0')
+		return (0);
+	else
 		return (1 + _strlen_recursion(s + 1));
-	}
-	return (0);
 }
