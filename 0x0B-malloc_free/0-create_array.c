89e5a8a4a02ae0aeecc81fd0ca1e11ea25986638
adamubida <mailadamu@gmail.com>
2022-10-03 14:44:03 +0100

    crtarry

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,29 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * *create_array - multiply the args
+ * @size: size of memory to be allocated
+ * @c: character to be placed in memory
  *
- * Return: Nothing.
+ * Return: 0 for successful exit
  */
+
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
 
-  if (size == 0)
-      return (NULL);
+	char *arrayOfChar;
+	unsigned int i;
 
-  char *s = (char*)malloc(size * sizeof(char));
+	arrayOfChar = malloc(size);
 
-  for (i = 0; i < size; i++)
-    s[i] = c;
+	if (size == 0 || arrayOfChar == 0)
+		return (0);
 
-  return (s);
+	for (i = 0; i < size; i++)
+	{
+		arrayOfChar[i] = c;
+	}
+
+	return (arrayOfChar);
 }
