227c8805e62e33078e722fa3597ee2a58efb5f95
adamubida <mailadamu@gmail.com>
2022-10-06 16:59:38 +0100

    mull

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,68 +1,29 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 /**
-  * _isdigit - tells if the string consists of digits
-  * @argv: pointer to current item in argument
-  * Return: return 0 if all digits, 1 if not all digits.
-  */
-int _isdigit(char *argv)
-{
-	int i;
-
-	i = 0;
-	while (argv[i])
-	{
-		if (argv[i] >= '0' && argv[i] <= '9')
-			i++;
-		else
-			return (1);
-	}
-	return (0);
-}
-/**
-  * _atoi - converts a string of ascii digits to the values they represent
-  * @s: pointer to the source string
-  * Return: value of digits
-  */
-int _atoi(char *s)
-{
-	int i, result;
-
-	i = result = 0;
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		i++;
-	}
-	return (result);
-}
-/**
-  * main - main function call
-  * @argc: argument count
-  * @argv: 2D array of arguments
-  * Return: return 0 on success, 98 on failure
-  */
+ * main - Entry Point
+ * atoi is a function that converts a string into an int
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, res = 1;
 
-	malloc();
 	if (argc != 3)
 	{
-		printf("Error\n");
-		exit(98);
+		printf("%s\n", "Error");
+		return (1);
 	}
-	for (i = 1; i < argc; i++)
+	else
 	{
-		if (_isdigit(argv[i]))
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			exit(98);
+			res *= atoi(argv[i]);
 		}
+		printf("%d\n", res);
 	}
 	return (0);
 }
