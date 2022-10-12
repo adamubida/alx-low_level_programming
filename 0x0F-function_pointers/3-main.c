8a31dfac2e19b92e1b6e1ea4537d57f35c72a85f
adamubida <mailadamu@gmail.com>
2022-10-12 09:23:06 +0100

    mainch

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,21 +1,39 @@
-#ifndef CALC_H
-#define CALC_H
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * struct op - Struct op
- *
- * @op: operator
- * @f: function
- */
-typedef struct op
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
 {
+	int num1, num2;
 	char *op;
-	int (*f)(int a, int b);
-} op_t;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-#endif
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
