dcbf125c30014208b15573a983f2e9c441ae76f0
adamubida <mailadamu@gmail.com>
2022-10-13 15:46:13 +0100

    prntstr

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,34 +1,29 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include<stdio.h>
 /**
- * print_strings - print strings.
-(* a blank line
- *@separator: the number of parameters
- *@n: the number of strings
-* Description: this function prints all the parameters)?
-(* section header: the header of this function is variadic_functions.h)*
-* Return: no retunr.
-*/
+ * print_strings - print strings
+ * @separator:char
+ * @n:unsigned int
+ * Return:void
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list list;
 	unsigned int i;
-	va_list strings;
-	char *string;
+	char *str;
 
-	va_start(strings, n);
-
-	i = 0;
-	while (i < n)
+	va_start(list, n);
+	for (i = 0; i < n; i++)
 	{
-		string = va_arg(strings, char*);
-		if (string == NULL)
-			printf("(nil)");
+		str = va_arg(list, char *);
+		if (str)
+			printf("%s", str);
 		else
-			printf("%s", string);
-		if (i != (n - 1) && separator != NULL)
+			printf("(nil)");
+		if (i < n - 1 && separator)
 			printf("%s", separator);
-		i++;
 	}
+	va_end(list);
 	printf("\n");
-
-	va_end(strings);
 }
