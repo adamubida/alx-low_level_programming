ad59cc641d21425609b4b82afbe2ae7a1edfe36d
adamubida <mailadamu@gmail.com>
2022-10-13 15:30:17 +0100

    prin

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,31 +1,47 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - print all the parametes.
-(* a blank line
- *@separator: the number of parameters
- *@n: the number of parameters
-* Description: this function prints all the parameters)?
-(* section header: the header of this function is variadic_functions.h)*
-* Return: no retunr.
-*/
-
-void print_numbers(const char *separator, const unsigned int n, ...)
-
+ * print_number - Function that prints an integer.
+ * @n: int type number
+ * Description: Can only use _putchar to print.
+ */
+void print_number(int n)
 {
-	va_list num;
-	unsigned int i;
+	long m; /* power of 10 */
+	int c; /* boolean check */
+	long num; /* convert int to long */
 
-	i = 0;
-
-	va_start(num, n);
-	while (i < n)
+	num = n;
+	/* negatives */
+	if (num < 0)
 	{
-		printf("%d", va_arg(num, unsigned int));
-		if (i < (n - 1) && separator != NULL)
-			printf("%s", separator);
-		i++;
+		num *= -1;
+		_putchar('-');
 	}
-	va_end(num);
-	printf("\n");
+
+	/* count up */
+	m = 1;
+	c = 1;
+	while (c)
+	{
+		if (num / (m * 10) > 0)
+			m *= 10;
+		else
+			c = 0;
+	}
+
+	/* count down */
+	while (num >= 0)
+	{
+		if (m == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / m % 10) + '0');
+			m /= 10;
+		}
+	}
 }
