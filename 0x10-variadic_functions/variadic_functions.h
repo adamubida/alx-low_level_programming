4e29267a6a82bef4525a4eb885beddab7a2f120d
adamubida <mailadamu@gmail.com>
2022-10-13 14:54:22 +0100

    var

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,8 @@
-ifndef VARIAFUNC
-#define VARIAFUNC
-
-
-#include <stdarg.h>
-#include <stdio.h>
-/**
- * struct args_t - contain the type and the function.
- * @format: the format to print
- * @function: the functions that prints
- *
- * Description: contains the type and the format to print.
- */
-typedef struct args_t
-{
-	char *format;
-	void (*function)();
-} args_t;
-
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-#endif /* VARIAFUNC */
+#endif
