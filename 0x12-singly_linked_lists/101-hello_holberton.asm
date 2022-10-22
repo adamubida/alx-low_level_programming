4a79a21a5661af70ac73cf335815dc30285afcb2
adamubida <mailadamu@gmail.com>
2022-10-22 15:32:39 +0100

    101holber

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,27 @@
-section     .text
-    global      main
+; My comments: It is so fun to write in assembly language
+; File: 101-hello_holberton.asm
+; Auth: Kirubel Alemu
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
+
+extern printf
+
+section .text
+   global main
 
 main:
-    mov     edx,len
-    mov     ecx,msg
-    mov     ebx,1
-    mov     eax,4
-    int     0x80
+   push rbp
 
-section     .data
-msg     db  'Hello, Holberton',0xa
-	len     equ $ - msg
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
